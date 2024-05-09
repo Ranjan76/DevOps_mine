c994ac3f907f2346c83020356d81498ca5443ed6
RANJAN"git config --global user.name RANJAN <lane.jogger@gmail.com>
2024-05-09 16:54:33 +0000

    ho gaye

--- 3-jenkins/3.1-simple-java-program/HelloWorld.java
+++ 3-jenkins/3.1-simple-java-program/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Hello, World From Varun Manik on date MaJan 13 2024");
+	System.out.println("Hello, World From Varun Manik on date dil 13 2026");
   }
-}
+} 
