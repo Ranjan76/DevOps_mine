2f52c97d58fac747fa0b72d29130d958bb0edd4c
Ranjan KUmar <ranjan@gmail.com>
2024-06-05 11:29:19 +0000

    date modified

--- 3-jenkins/3.1-simple-java-program/HelloWorld.java
+++ 3-jenkins/3.1-simple-java-program/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Hello, World From Varun Manik on date May 27 2026");
+	System.out.println("Hello, World From Varun Manik on date JUne 5 2024");
   }
 } 
