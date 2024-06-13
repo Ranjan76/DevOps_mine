79e57a1c410039abba79d6019f4705745aabc5c6
Your Name <you@example.com>
2024-06-13 05:49:42 +0000

    file deleted

--- 4-ansible-terraform/4.1-tf-Ranjan-localfile/main.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "Index_html" {
-  content  = "Hi, How are you?"
-  filename = "index.html"
-}
--- 4-ansible-terraform/4.1-tf-loacl-file/main.tf
+++
@@ -1,5 +0,0 @@
-resource "local_file" "Index_html" {
-  content  = "Hi, How are you?"
-  filename = "Index.html"
-}
-
