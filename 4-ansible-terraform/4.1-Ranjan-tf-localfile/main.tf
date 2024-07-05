a1f881233cd4c6073223fd5f75da9072ee4bb1e9
Your Name <you@example.com>
2024-07-05 15:42:46 +0530

    file delted

--- 4-ansible-terraform/4.1-Ranjan-tf-localfile/main.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "Index_html" {
-  content  = "Hi, Dost, How are you? My friend reply dost, I am fine and I am doing good.foo!"
-  filename = "Index.html"
-}
--- 4-ansible-terraform/4.1-tf-Ranjan-localfile/main.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "Index_html" {
-  content  = "Hi, I am Ranjan. How are ypu doing? Reply from Machine that I am foo!"
-  filename = "index.html"
-}
