6794f429e68f5b3d05d8fac323143e1a3fad6e0b
Your Name <you@example.com>
2024-10-19 04:06:21 +0000

    i changed some date

--- 3-jenkins/3.1-simple-java-program/HelloWorld.java
+++ 3-jenkins/3.1-simple-java-program/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Hello, World From Varun Manik on date JUne 5 2024");
+	System.out.println("Hello, World From Ranjan Kumar on date October 19 2024");
   }
 } 
